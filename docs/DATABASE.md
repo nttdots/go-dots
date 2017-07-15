902bdc202f556fb61db8f98e7804935f9d565994
Kouji Okada <okd@lepidum.co.jp>
2017-07-16 00:44:22 +0900

    Update the parts of documents related to the database configurations.

---
+++ docs/DATABASE.md
@@ -0,0 +1,67 @@
+# Setting up databases
+
+In this document, we describe how to configure the data in the database.
+
+## Editing the data on the database 
+
+For the current implementation, some part of the system configuration is done by editing the data on the database directly. Edit dots_server/db_models/template.sql to fit in your environment. Those configurations are blocker configurations and customer configurations. We detail each configration in the following sections.
+
+After completing the edit, you can update the data in the database by the command below.
+
+mysql -u root dots < dots_server/db_models/template.sql 
+
+## Blocker configuration
+
+The blockers are the entities to mitigate the DDoS attacks based on the requests from the dots_server. The blocker related tables are below:
+
+* 'blocker': Basic blocker information
+* 'blocker_parameter': Additinal configuration for the blockers
+* 'login_profile': The login profile for the blocker. The current implementation does not utilize these profile.
+
+The tables you have to edit here are 'blocker' and 'blocker_parameter'.
+
+### 'blocker' table
+
+The current implementation only support the blocker type of 'GoBGP-RTBH'. So set the type field of every blockers to 'GoBGP-RTBH'. In the capacity field, you can specify the strength of the blocker. The capacity is the number of the entries the blocker can deal with. The load field is the number of the entries the blocker is now dealing with and will be updated by the system. You should set to 0 for the initial configuration.
+
+### 'blocker_parameter' table
+
+The 'blocker_parameter' tables is for the further configurations of each blocker. 'blocker_parameter' has the key/value structure. The parameter has 3 keys below:
+
+* nextHop: the nextHop IP address for the RTBH configuration. If you configure the nextHop as '0.0.0.0', the blocker simply blackhole the attack traffics.
+* host: IP address or FQDN of the blocker.
+* port: port number of the controll access.
+
+## Customer configuration
+
+Our system utilizes the concept of 'customer' to validate (mainly mitigation) requests as described in the "How it works" section of [README](../README.md).  The customer related tables are listed below.
+
+* 'customer': Basic blocker information
+* 'customer_common_name': customer certificate common names
+* 'parameter_value': message parameters which can be utilized for the message validations
+* 'prefix': IP address prefix information which can be utilized for the message validations
+
+We detail each table in the following sections.
+
+### 'customer'
+
+There is no special instruction for this table.
+
+### 'customer_common_name' table
+
+ To validate each messages, first we check the common name field of the client certificates. To validate the common names, we utilize this table.
+
+### 'parameter_value'
+
+This tables is to store the identifier parameters and mitigation scope parameters. If you specify the customer_id field appropriately, the entry is utilized for the mitigation request validation for the customer specified by the customer_id.
+
+The types of parameters for the validation use are below.
+
+* FQDN: valid FQDN(s) for the customer
+* URI: valid URI(s) for the customer
+* E_164: valid E.164 nubmers for the cusotmer
+
+### 'prefix'
+
+The 'prefix' table is to store the IP address information. If the mitigation scopes in the mitigation requests includes the IP address prefixes, the message validator validates the messages by checking whether the database has the entries which has the 'type' field is 'ADDRESS_RANGE' and the CIDR block specified by the 'addr' field and the 'prefix_len' matches to the IP address block specified by the mitigation scope of the message.
+
--- dots_server/db_models/template.sql
+++ dots_server/db_models/template.sql
@@ -184,12 +184,11 @@ CREATE TABLE `parameter_value` (
 
 ####### Add an insert query to add customer related parameters.
 ####### these parameters are to store the identifier parameters or mitigation scope parameters.
-####### Basically this table is modified by the system only.
 #  id: id of this login profile.
-#  customer_id: id of the customer this parameter corresponds to.
-#  identifier_id: id of the customer identifier. if this parameter is not for an identifier, set this field to '0'.
-#  mitigation_scope_id: id of a mitigation scope the customer requested. if this parameter is not for a mitigation scope, set this field to '0'. Note that this id is not the 'mitigation_id', but the database id.
-#  type: type of this parameter. these are based on drafts of the data channel and the signal channel.
+#  customer_id: id of the customer this parameter corresponds to. If specified, this parameter object is used for the validation use.
+#  identifier_id: id of the customer identifier basically set by the system. if this parameter object is not for an identifier, set this field to '0'.
+#  mitigation_scope_id: id of a mitigation scope the customer requested  basically set by the system. if this parameter object is not for a mitigation scope, set this field to '0'. Note that this id is not the 'mitigation_id', but the database id.
+#  type: type of this parameter. these are based on the internet drafts.
 #   'TARGET_PROTOCOL','FQDN','URI','E_164','TRAFFIC_PROTOCOL','ALIAS'
 #  string_value: if this parameter is a type of string parameter, specify the value in the string format.
 #  int_value: if this parameter is a type of int parameter, specify the value in the integer format.
@@ -243,7 +242,31 @@ CREATE TABLE `prefix` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
-####### Basically the table 'prefix' is modified by the system only.
+####### Add an insert query to add customer related parameters.
+#  id: id of this login profile.
+#  customer_id: id of the customer this prefix corresponds to. If specified, this prefix object is used for the validation use.
+#  identifier_id: id of the customer identifier. basically set by the system. if this prefix object is not for an identifier, set this field to '0'.
+#  mitigation_scope_id: id of a mitigation scope the customer requested. basically set by the system. if this prefix object is not for a mitigation scope, set this field to '0'. Note that this id is not the 'mitigation_id', but the database id.
+#  access_control_list_entry_id`: id of an access control list the customer requested. basically set by the system. if this parameter object is not for a mitigation scope, set this field to '0'. Note that this id is not the 'mitigation_id', but the database id.
+#  type: type of this parameter. part of these are based on drafts of the data channel and the signal channel.
+#    'TARGET_PREFIX','SOURCE_IPV4_NETWORK','DESTINATION_IPV4_NETWORK','IP','PREFIX','ADDRESS_RANGE','IP_ADDRESS','TARGET_IP'
+#  addr: address of the prefix
+#  prefix_len: length of the prefix
+#
+# INSERT INTO `prefix` (`id`, `customer_id`, `identifier_id`, `mitigation_scope_id`, `blocker_id`, `type`, `addr`, `prefix_len`, `created`, `updated`)
+#  VALUES
+#    (1,1,0,0,0,'ADDRESS_RANGE','192.168.1.0',24,'2017-04-13 13:44:34','2017-04-13 13:44:34'),
+#    (5,2,0,0,0,'ADDRESS_RANGE','127.0.0.1',32,'2017-04-13 13:44:34','2017-04-13 13:44:34'),
+#    (6,2,0,0,0,'ADDRESS_RANGE','2002:db8:6401::',64,'2017-04-13 13:44:34','2017-04-13 13:44:34'),
+#    (7,2,0,0,0,'ADDRESS_RANGE','2002:db8:6402::',64,'2017-04-13 13:44:34','2017-04-13 13:44:34'),
+#    (8,2,0,0,0,'ADDRESS_RANGE','192.168.7.0',24,'2017-04-13 13:44:34','2017-04-13 13:44:34'),
+#    (9,3,0,0,0,'ADDRESS_RANGE','129.0.0.1',32,'2017-04-13 13:44:34','2017-04-13 13:44:34'),
+#    (10,3,0,0,0,'ADDRESS_RANGE','2003:db8:6401::',64,'2017-04-13 13:44:34','2017-04-13 13:44:34'),
+#    (11,3,0,0,0,'ADDRESS_RANGE','2003:db8:6402::',64,'2017-04-13 13:44:34','2017-04-13 13:44:34'),
+#    (12,0,0,1,0,'TARGET_IP','2002:db8:6401::',64,'2017-04-13 13:44:34','2017-04-13 13:44:34'),
+#    (13,0,0,1,0,'TARGET_PREFIX','2002:db8:6401::',64,'2017-04-13 13:44:34','2017-04-13 13:44:34'),
+#    (14,0,0,2,0,'TARGET_IP','2002:db8:6402::',64,'2017-04-13 13:44:34','2017-04-13 13:44:34'),
+#    (15,0,0,2,0,'TARGET_PREFIX','2002:db8:6402::',64,'2017-04-13 13:44:34','2017-04-13 13:44:34');
 
 
 # mitigation_scope
