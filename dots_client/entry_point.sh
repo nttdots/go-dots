c15046d5ffef7212f2e42b3ecfc41c48e15f1cac
__kaname__ <kaname@nttv6.jp>
2017-07-18 14:32:08 +0200

    Update entry_point.sh

--- dots_client/entry_point.sh
+++ dots_client/entry_point.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-# /usr/bin/wait-for-it.sh -t 30 ${DOTS_SERVER_IPV4}:8080 || exit 1
-
-dots_client -vv -server ${DOTS_SERVER_IPV4} -signalChannelPort ${SIGNAL_CHANNEL_PORT} -dataChannelPort ${DATA_CHANNEL_PORT} -clientCertFile ${GOPATH}/src/github.com/nttdots/go-dots/certs/client-cert.pem -certFile ${GOPATH}/src/github.com/nttdots/go-dots/certs/ca-cert.pem -clientKeyFile ${GOPATH}/src/github.com/nttdots/go-dots/certs/client-key.pem
+dots_client -vv -server ${DOTS_SERVER} -signalChannelPort ${SIGNAL_CHANNEL_PORT} -dataChannelPort ${DATA_CHANNEL_PORT} -clientCertFile ${GOPATH}/src/github.com/nttdots/go-dots/certs/client-cert.pem -certFile ${GOPATH}/src/github.com/nttdots/go-dots/certs/ca-cert.pem -clientKeyFile ${GOPATH}/src/github.com/nttdots/go-dots/certs/client-key.pem
 while :
 do
     sleep 1
