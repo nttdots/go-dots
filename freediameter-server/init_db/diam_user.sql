186d44b9a5871c53fb10f57fe41fe6249e8dcc3c
taka <taka@fullmarks.co.jp>
2017-09-26 11:39:24 +0900

    diameterは不要なため削除

--- freediameter-server/init_db/diam_user.sql
+++
@@ -1,7 +0,0 @@
-insert into grp(id, grp_name) values (1, 'group1');
-
-insert into users(id, username, eapmethod, password) values (1, 'client1@example.com', 13, '*');
-insert into users(id, username, eapmethod, password) values (2, 'client2@example.com', 13, '*');
-
-insert into user_grp(user, grp) values (1, 1);
-insert into user_grp(user, grp) values (2, 1);
--- freediameter-server/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-/usr/bin/wait-for-it.sh -t 30 diameap_db:3306 || exit 1
-
-/usr/bin/freeDiameterd -d --dbg_gnutls 9 -c /config/freeDiameter.conf
