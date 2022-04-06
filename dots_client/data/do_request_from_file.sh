bf224087f7a0e5bc2830ab9407feffa8d6fc09fa
lieunguyen-tma <nttlieu@tma.com.vn>
2022-04-06 13:43:08 +0700

    Update do_request_from_file.sh to add option cert files #388

--- dots_client/data/do_request_from_file.sh
+++ dots_client/data/do_request_from_file.sh
@@ -1,21 +1,55 @@
-#!/bin/sh
+#!/bin/bash
+CA_CERT=../certs/ca-cert.pem
+CLIENT_CERT=../certs/client-cert.pem
+CLIENT_KEY=../certs/client-key.pem
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        --ca-cert)
+            CA_CERT=$2
+            shift 2
+            ;;
+        --client-cert)
+            CLIENT_CERT=$2
+            shift 2
+            ;;
+        --client-key)
+            CLIENT_KEY=$2
+            shift 2
+            ;;
+        POST|PUT)
+            METHOD=$1
+            URI_PATH=$2
+            FILE=$3
+            shift 3
+            ;;
+        GET|DELETE)
+            METHOD=$1
+            URI_PATH=$2
+            shift 2
+            ;;
+        *)
+            echo "Unknown option $1"
+            exit 1
+            ;;
+    esac
+done
 
-CERTS_DIR=$1
-METHOD=$2
-URI_PATH=$3
-FILE=$4
+if { [ -n "$CA_CERT" ] && ! [ -e "$CA_CERT" ]; }; then
+    echo "$CA_CERT is not existed"
+    exit 1
+fi
+if { [ -n "$CLIENT_CERT" ] && ! [ -e "$CLIENT_CERT" ]; }; then
+    echo "$CLIENT_CERT is not existed"
+    exit 1
+fi
+if { [ -n "$CLIENT_KEY" ] && ! [ -e "$CLIENT_KEY" ]; }; then
+    echo "$CLIENT_KEY is not existed"
+    exit 1
+fi
 
-if [ -z "$METHOD" ]; then
-  echo 'No METHOD parameter.' >&2
-  exit 1
-fi
-if [ -z "$URI_PATH" ]; then
-  echo 'No URI_PATH parameter.' >&2
-  exit 1
-fi
 if { [ -n "$FILE" ] && ! [ -e "$FILE" ]; }; then
-  echo "Input file $FILE is not existed"
-  exit 1
+    echo "$FILE is not existed"
+    exit 1
 fi
 
 if [ -n "$FILE" ]; then
@@ -23,9 +57,9 @@ wget \
   -q -S -O - \
   --content-on-error \
   --no-check-certificate \
-  --ca-certificate="$CERTS_DIR"/ca-cert.pem \
-  --certificate="$CERTS_DIR"/client-cert.pem \
-  --private-key="$CERTS_DIR"/client-key.pem \
+  --ca-certificate=$CA_CERT \
+  --certificate=$CLIENT_CERT \
+  --private-key=$CLIENT_KEY \
   --method="$METHOD" \
   "$URI_PATH" \
   "--body-file=$FILE" \
@@ -35,9 +69,9 @@ wget \
   -q -S -O - \
   --content-on-error \
   --no-check-certificate \
-  --ca-certificate="$CERTS_DIR"/ca-cert.pem \
-  --certificate="$CERTS_DIR"/client-cert.pem \
-  --private-key="$CERTS_DIR"/client-key.pem \
+  --ca-certificate=$CA_CERT \
+  --certificate=$CLIENT_CERT \
+  --private-key=$CLIENT_KEY \
   --method="$METHOD" \
   "$URI_PATH"
 fi
